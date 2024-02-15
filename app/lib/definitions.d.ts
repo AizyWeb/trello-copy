3ef8f9175b2be19e44f142b13be1c124480c97d3
AizyWeb <jimenezcarballoane@gmail.com>
2024-02-15 14:09:14 +0100

    Defining types

---
+++ app/lib/definitions.d.ts
@@ -0,0 +1,20 @@
+export type Card = {
+    title: string
+    description: string
+    tasks: string[]
+    priority: 'low' | 'medium' | 'high'
+    completedPercentaje: number
+}
+
+export type Board = {
+    id: string
+    cards: Card[]
+    users_id: string[]
+}
+
+export type User = {
+    id: string
+    username: string
+    password: string
+    boards: Board[]
+}
